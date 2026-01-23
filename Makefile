akyriako/typesense-operator

commit d6c10d27c92c8a9fd691bc8c888d068cf7a80e03

Improvement: avoid downgrades by syncing configmaps faster and waiting when they change (#164)
* delay reconciliation on configmap updates

* flip logic for reconcile

* kubelet info

* missing err check

* len of nil slice is 0

* implement annotation update for configmap sync

* fix label

* simpler logic

* prohibit post-boostrap downgrade, reinstate quorum reconciliation, code restructuring #164

---------

Co-authored-by: Kyriakos Akriotis <akriotis.kyriakos@gmail.com>

config/samples/ts_v1alpha1_typesensecluster_kind.yaml
@@ -35,8 +35,15 @@ metadata:
   name: c-kind-1
 spec:
   image: typesense/typesense:30.0.rc7-amd64
-  replicas: 5
+  replicas: 3
   corsDomains: "http://localhost,https://www.example.de"
+  resources:
+    limits:
+      cpu: "1200m"
+      memory: "1024Mi"
+    requests:
+      cpu: "100m"
+      memory: "64Mi"
   storage:
     size: 150Mi
     storageClassName: typesense-local-path
@@ -47,107 +54,107 @@ spec:
   additionalServerConfiguration:
     name: c-kind-1-server-configuration
 ---
-apiVersion: v1
-kind: ConfigMap
-metadata:
-  name: c-kind-2-config
-data:
-  TYPESENSE_ENABLE_SEARCH_ANALYTICS: "true"
----
-apiVersion: v1
-kind: Secret
-metadata:
-  labels:
-    app.kubernetes.io/name: typesense-operator
-    app.kubernetes.io/managed-by: kustomize
-  name: scraper-auth-docusaurus-example-com
-type: Opaque
-data:
-  KC_URL: VGhpc0lzTm90QVNlY3VyZVBhc3N3b3Jk
-  KC_REALM: VGhpc0lzTm90QVNlY3VyZVBhc3N3b3Jk
-  KC_CLIENT_ID: VGhpc0lzTm90QVNlY3VyZVBhc3N3b3Jk
-  KC_CLIENT_SECRET: VGhpc0lzTm90QVNlY3VyZVBhc3N3b3Jk
----
-apiVersion: ts.opentelekomcloud.com/v1alpha1
-kind: TypesenseCluster
-metadata:
-  labels:
-    app.kubernetes.io/name: typesense-operator
-    app.kubernetes.io/managed-by: kustomize
-  name: c-kind-2
-spec:
-  image: typesense/typesense:29.0
-  replicas: 7
-  adminApiKey:
-    name: typesense-common-bootstrap-key
-  enableCors: true
-  apiPort: 8108
-  healthProbeTimeoutInMilliseconds: 1500
-  resources:
-    limits:
-      cpu: "200m"
-      memory: "1024Mi"
-    requests:
-      cpu: "100m"
-      memory: "64Mi"
-  storage:
-    size: 75Mi
-    storageClassName: typesense-local-path
-  metrics:
-    release: promstack
-  ingress:
-    referer: referer.example.com
-    host: host.example.com
-    path: /
-    pathType: ImplementationSpecific
-    ingressClassName: nginx
-    clusterIssuer: lets-encrypt-prod
-#    readOnlyRootFilesystem:
-#      volumes:
-#        - name: nginx-var-cache
-#          emptyDir: {}
-#        - name: run
-#          emptyDir: {}
-#      volumeMounts:
-#        - name: nginx-var-cache
-#          mountPath: /var/cache/nginx
-#        - name: run
-#          mountPath: /run
----
+#apiVersion: v1
+#kind: ConfigMap
+#metadata:
+#  name: c-kind-2-config
+#data:
+#  TYPESENSE_ENABLE_SEARCH_ANALYTICS: "true"
+#---
+#apiVersion: v1
+#kind: Secret
+#metadata:
+#  labels:
+#    app.kubernetes.io/name: typesense-operator
+#    app.kubernetes.io/managed-by: kustomize
+#  name: scraper-auth-docusaurus-example-com
+#type: Opaque
+#data:
+#  KC_URL: VGhpc0lzTm90QVNlY3VyZVBhc3N3b3Jk
+#  KC_REALM: VGhpc0lzTm90QVNlY3VyZVBhc3N3b3Jk
+#  KC_CLIENT_ID: VGhpc0lzTm90QVNlY3VyZVBhc3N3b3Jk
+#  KC_CLIENT_SECRET: VGhpc0lzTm90QVNlY3VyZVBhc3N3b3Jk
+#---
 #apiVersion: ts.opentelekomcloud.com/v1alpha1
 #kind: TypesenseCluster
 #metadata:
 #  labels:
 #    app.kubernetes.io/name: typesense-operator
 #    app.kubernetes.io/managed-by: kustomize
-#  name: c-kind-3
+#  name: c-kind-2
 #spec:
-#  image: typesense/typesense:27.1
-#  replicas: 1
-#  apiPort: 18108
-#  peeringPort: 18107
-#  enableCors: true
-#  corsDomains: "http://localhost,https://www.example.de"
-#  storage:
-#    size: 10Mi
-#    storageClassName: typesense-local-path
+#  image: typesense/typesense:29.0
+#  replicas: 7
 #  adminApiKey:
 #    name: typesense-common-bootstrap-key
+#  enableCors: true
+#  apiPort: 8108
+#  healthProbeTimeoutInMilliseconds: 1500
+#  resources:
+#    limits:
+#      cpu: "200m"
+#      memory: "1024Mi"
+#    requests:
+#      cpu: "100m"
+#      memory: "64Mi"
+#  storage:
+#    size: 75Mi
+#    storageClassName: typesense-local-path
+#  metrics:
+#    release: promstack
 #  ingress:
-#    host: www.example.de
+#    referer: referer.example.com
+#    host: host.example.com
+#    path: /
+#    pathType: ImplementationSpecific
 #    ingressClassName: nginx
-#    clusterIssuer: opentelekomcloud-letsencrypt-staging
-#    resources:
-#      limits:
-#        cpu: "300m"
-#        memory: "128Mi"
-#      requests:
-#        cpu: "100m"
-#        memory: "32Mi"
-#  scrapers:
-#    - name: docusaurus-example-com
-#      image: typesense/docsearch-scraper:0.11.0
-#      authConfiguration:
-#        name: scraper-auth-docusaurus-example-com
-#      config: "{\"index_name\":\"docuraurus-example\",\"start_urls\":[\"https://docusaurus.example.com/\"],\"sitemap_urls\":[\"https://docusaurus.example.com/sitemap.xml\"],\"sitemap_alternate_links\":true,\"stop_urls\":[\"/tests\"],\"selectors\":{\"lvl0\":{\"selector\":\"(//ul[contains(@class,'menu__list')]//a[contains(@class, 'menu__link menu__link--sublist menu__link--active')]/text() | //nav[contains(@class, 'navbar')]//a[contains(@class, 'navbar__link--active')]/text())[last()]\",\"type\":\"xpath\",\"global\":true,\"default_value\":\"Documentation\"},\"lvl1\":\"header h1\",\"lvl2\":\"article h2\",\"lvl3\":\"article h3\",\"lvl4\":\"article h4\",\"lvl5\":\"article h5, article td:first-child\",\"lvl6\":\"article h6\",\"text\":\"article p, article li, article td:last-child\"},\"strip_chars\":\" .,;:#\",\"custom_settings\":{\"separatorsToIndex\":\"_\",\"attributesForFaceting\":[\"language\",\"version\",\"type\",\"docusaurus_tag\"],\"attributesToRetrieve\":[\"hierarchy\",\"content\",\"anchor\",\"url\",\"url_without_anchor\",\"type\"]},\"conversation_id\":[\"833762294\"],\"nb_hits\":46250}"
-#      schedule: '*/2 * * * *'
+#    clusterIssuer: lets-encrypt-prod
+##    readOnlyRootFilesystem:
+##      volumes:
+##        - name: nginx-var-cache
+##          emptyDir: {}
+##        - name: run
+##          emptyDir: {}
+##      volumeMounts:
+##        - name: nginx-var-cache
+##          mountPath: /var/cache/nginx
+##        - name: run
+##          mountPath: /run
+#---
+##apiVersion: ts.opentelekomcloud.com/v1alpha1
+##kind: TypesenseCluster
+##metadata:
+##  labels:
+##    app.kubernetes.io/name: typesense-operator
+##    app.kubernetes.io/managed-by: kustomize
+##  name: c-kind-3
+##spec:
+##  image: typesense/typesense:27.1
+##  replicas: 1
+##  apiPort: 18108
+##  peeringPort: 18107
+##  enableCors: true
+##  corsDomains: "http://localhost,https://www.example.de"
+##  storage:
+##    size: 10Mi
+##    storageClassName: typesense-local-path
+##  adminApiKey:
+##    name: typesense-common-bootstrap-key
+##  ingress:
+##    host: www.example.de
+##    ingressClassName: nginx
+##    clusterIssuer: opentelekomcloud-letsencrypt-staging
+##    resources:
+##      limits:
+##        cpu: "300m"
+##        memory: "128Mi"
+##      requests:
+##        cpu: "100m"
+##        memory: "32Mi"
+##  scrapers:
+##    - name: docusaurus-example-com
+##      image: typesense/docsearch-scraper:0.11.0
+##      authConfiguration:
+##        name: scraper-auth-docusaurus-example-com
+##      config: "{\"index_name\":\"docuraurus-example\",\"start_urls\":[\"https://docusaurus.example.com/\"],\"sitemap_urls\":[\"https://docusaurus.example.com/sitemap.xml\"],\"sitemap_alternate_links\":true,\"stop_urls\":[\"/tests\"],\"selectors\":{\"lvl0\":{\"selector\":\"(//ul[contains(@class,'menu__list')]//a[contains(@class, 'menu__link menu__link--sublist menu__link--active')]/text() | //nav[contains(@class, 'navbar')]//a[contains(@class, 'navbar__link--active')]/text())[last()]\",\"type\":\"xpath\",\"global\":true,\"default_value\":\"Documentation\"},\"lvl1\":\"header h1\",\"lvl2\":\"article h2\",\"lvl3\":\"article h3\",\"lvl4\":\"article h4\",\"lvl5\":\"article h5, article td:first-child\",\"lvl6\":\"article h6\",\"text\":\"article p, article li, article td:last-child\"},\"strip_chars\":\" .,;:#\",\"custom_settings\":{\"separatorsToIndex\":\"_\",\"attributesForFaceting\":[\"language\",\"version\",\"type\",\"docusaurus_tag\"],\"attributesToRetrieve\":[\"hierarchy\",\"content\",\"anchor\",\"url\",\"url_without_anchor\",\"type\"]},\"conversation_id\":[\"833762294\"],\"nb_hits\":46250}"
+##      schedule: '*/2 * * * *'

internal/controller/typesensecluster_configmap.go
@@ -3,6 +3,10 @@ package controller
 import (
 	"context"
 	"fmt"
+	"sort"
+	"strings"
+	"time"
+
 	tsv1alpha1 "github.com/akyriako/typesense-operator/api/v1alpha1"
 	appsv1 "k8s.io/api/apps/v1"
 	v1 "k8s.io/api/core/v1"
@@ -12,10 +16,13 @@ import (
 	"k8s.io/utils/ptr"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"strings"
 )
 
-func (r *TypesenseClusterReconciler) ReconcileConfigMap(ctx context.Context, ts tsv1alpha1.TypesenseCluster) (updated *bool, err error) {
+const (
+	forceConfigMapUpdateAnnotationKey = "ts.opentelekomcloud.com/forced-configmap-update-time"
+)
+
+func (r *TypesenseClusterReconciler) ReconcileConfigMap(ctx context.Context, ts tsv1alpha1.TypesenseCluster) (*bool, error) {
 	r.logger.V(debugLevel).Info("reconciling config map")
 
 	configMapName := fmt.Sprintf(ClusterNodesConfigMap, ts.Name)
@@ -23,33 +30,33 @@ func (r *TypesenseClusterReconciler) ReconcileConfigMap(ctx context.Context, ts 
 	configMapObjectKey := client.ObjectKey{Namespace: ts.Namespace, Name: configMapName}
 
 	var cm = &v1.ConfigMap{}
-	if err = r.Get(ctx, configMapObjectKey, cm); err != nil {
+	if err := r.Get(ctx, configMapObjectKey, cm); err != nil {
 		if apierrors.IsNotFound(err) {
 			configMapExists = false
 		} else {
 			r.logger.Error(err, fmt.Sprintf("unable to fetch config map: %s", configMapName))
-			return ptr.To[bool](false), err
+			return nil, err
 		}
 	}
 
 	if !configMapExists {
 		r.logger.V(debugLevel).Info("creating config map", "configmap", configMapObjectKey.Name)
 
-		cm, err = r.createConfigMap(ctx, configMapObjectKey, &ts)
+		_, err := r.createConfigMap(ctx, configMapObjectKey, &ts)
 		if err != nil {
 			r.logger.Error(err, "creating config map failed", "configmap", configMapObjectKey.Name)
 			return nil, err
 		}
-	} else {
-		r.logger.V(debugLevel).Info("updating config map", "configmap", configMapObjectKey.Name)
 
-		cm, _, err = r.updateConfigMap(ctx, &ts, cm, nil)
-		if err != nil {
-			return nil, err
-		}
+		return nil, nil
 	}
 
-	return &configMapExists, nil
+	_, _, updated, err := r.updateConfigMap(ctx, &ts, cm, nil, false)
+	if err != nil {
+		return ptr.To[bool](false), err
+	}
+
+	return &updated, nil
 }
 
 const nodeNameLenLimit = 64
@@ -81,7 +88,7 @@ func (r *TypesenseClusterReconciler) createConfigMap(ctx context.Context, key cl
 	return cm, nil
 }
 
-func (r *TypesenseClusterReconciler) updateConfigMap(ctx context.Context, ts *tsv1alpha1.TypesenseCluster, cm *v1.ConfigMap, replicas *int32) (*v1.ConfigMap, int, error) {
+func (r *TypesenseClusterReconciler) updateConfigMap(ctx context.Context, ts *tsv1alpha1.TypesenseCluster, cm *v1.ConfigMap, replicas *int32, resizeOp bool) (*v1.ConfigMap, int, bool, error) {
 	stsName := fmt.Sprintf(ClusterStatefulSet, ts.Name)
 	stsObjectKey := client.ObjectKey{
 		Name:      stsName,
@@ -93,13 +100,13 @@ func (r *TypesenseClusterReconciler) updateConfigMap(ctx context.Context, ts *ts
 		if apierrors.IsNotFound(err) {
 			err := r.deleteConfigMap(ctx, cm)
 			if err != nil {
-				return nil, 0, err
+				return nil, 0, false, err
 			}
 		} else {
 			r.logger.Error(err, fmt.Sprintf("unable to fetch statefulset: %s", stsName))
 		}
 
-		return nil, 0, err
+		return nil, 0, false, err
 	}
 
 	if replicas == nil {
@@ -107,15 +114,18 @@ func (r *TypesenseClusterReconciler) updateConfigMap(ctx context.Context, ts *ts
 	}
 
 	nodes, err := r.getNodes(ctx, ts, *replicas, false)
+	if err != nil {
+		return nil, 0, false, err
+	}
 	fallback, err := r.getNodes(ctx, ts, *replicas, true)
 	if err != nil {
-		return nil, 0, err
+		return nil, 0, false, err
 	}
 
 	availableNodes := len(nodes)
 	if availableNodes == 0 {
 		r.logger.V(debugLevel).Info("empty quorum configuration")
-		return nil, 0, fmt.Errorf("empty quorum configuration")
+		return nil, 0, false, fmt.Errorf("empty quorum configuration")
 	}
 
 	desired := cm.DeepCopy()
@@ -124,25 +134,66 @@ func (r *TypesenseClusterReconciler) updateConfigMap(ctx context.Context, ts *ts
 		"fallback": strings.Join(fallback, ","),
 	}
 
-	r.logger.V(debugLevel).Info("current quorum configuration", "size", availableNodes, "nodes", nodes)
+	if !resizeOp {
+		currentNodes := strings.Split(cm.Data["nodes"], ",")
+		sort.Strings(currentNodes)
+		r.logger.V(debugLevel).Info("current quorum configuration", "size", len(currentNodes), "nodes", currentNodes)
+	}
 
+	updated := false
 	if cm.Data["nodes"] != desired.Data["nodes"] || cm.Data["fallback"] != desired.Data["fallback"] {
-		r.logger.Info("updating quorum configuration", "size", availableNodes, "nodes", nodes)
+		if !resizeOp {
+			sort.Strings(nodes)
+			r.logger.Info("updating quorum configuration", "size", availableNodes, "nodes", nodes)
+		}
 
 		err := r.Update(ctx, desired)
 		if err != nil {
 			r.logger.Error(err, "updating quorum configuration failed")
-			return nil, 0, err
+			return nil, 0, false, err
 		}
+		updated = true
 	}
 
-	return desired, availableNodes, nil
+	return desired, availableNodes, updated, nil
 }
 
 func (r *TypesenseClusterReconciler) deleteConfigMap(ctx context.Context, cm *v1.ConfigMap) error {
 	err := r.Delete(ctx, cm)
 	if err != nil {
 		return err
+	}
+
+	return nil
+}
+
+// ForcePodsConfigMapUpdate forces a configmap update for all pods in the statefulset
+// it should be called after a configmap update occurs
+// https://kubernetes.io/docs/tasks/configure-pod-container/configure-pod-configmap/#mounted-configmaps-are-updated-automatically
+func (r *TypesenseClusterReconciler) forcePodsConfigMapUpdate(ctx context.Context, ts *tsv1alpha1.TypesenseCluster) error {
+	labelMap := getLabels(ts)
+	labelSelector := labels.SelectorFromSet(labelMap)
+
+	var podList v1.PodList
+	if err := r.Client.List(ctx, &podList,
+		client.InNamespace(ts.Namespace),
+		client.MatchingLabelsSelector{Selector: labelSelector},
+	); err != nil {
+		return err
+	}
+
+	for i := range podList.Items {
+		pod := &podList.Items[i]
+
+		if pod.Annotations == nil {
+			pod.Annotations = map[string]string{}
+		}
+		pod.Annotations[forceConfigMapUpdateAnnotationKey] = time.Now().Format(time.RFC3339)
+
+		if err := r.Patch(ctx, pod, client.MergeFrom(pod.DeepCopy())); err != nil {
+			r.logger.Error(err, "patching to pod annotations failed", "pod", pod.Name)
+			return err
+		}
 	}
 
 	return nil

internal/controller/typesensecluster_condition_types.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"context"
+
 	tsv1alpha1 "github.com/akyriako/typesense-operator/api/v1alpha1"
 	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -36,7 +37,7 @@ const (
 )
 
 func (r *TypesenseClusterReconciler) initConditions(ctx context.Context, ts *tsv1alpha1.TypesenseCluster) error {
-	if ts.Status.Conditions == nil || len(ts.Status.Conditions) == 0 {
+	if len(ts.Status.Conditions) == 0 {
 		if err := r.patchStatus(ctx, ts, func(status *tsv1alpha1.TypesenseClusterStatus) {
 			meta.SetStatusCondition(&ts.Status.Conditions, metav1.Condition{Type: ConditionTypeReady, Status: metav1.ConditionUnknown, Reason: ConditionReasonReconciliationInProgress, Message: InitReconciliationMessage})
 			status.Phase = "Bootstrapping"

internal/controller/typesensecluster_controller.go
@@ -19,6 +19,9 @@ package controller
 import (
 	"context"
 	"fmt"
+	"strings"
+	"time"
+
 	"github.com/go-logr/logr"
 	"github.com/pkg/errors"
 	"golang.org/x/text/cases"
@@ -33,8 +36,6 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/event"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/predicate"
-	"strings"
-	"time"
 
 	tsv1alpha1 "github.com/akyriako/typesense-operator/api/v1alpha1"
 )
@@ -68,8 +69,16 @@ var (
 			return !e.DeleteStateUnknown
 		},
 	})
+	// kubelets sync configmaps by default every minute so let's wait for 2 minutes
+	configMapRequeuePeriod = 2 * time.Minute
+	reconcileRequeuePeriod = 60 * time.Second
+)
 
-	requeueAfter = time.Second * 30
+type Action string
+
+const (
+	Bootstrapping Action = "bootstrapping"
+	Reconciling   Action = "reconciling"
 )
 
 // +kubebuilder:rbac:groups=ts.opentelekomcloud.com,resources=typesenseclusters,verbs=get;list;watch;create;update;patch;delete
@@ -123,7 +132,7 @@ func (r *TypesenseClusterReconciler) Reconcile(ctx context.Context, req ctrl.Req
 	}
 
 	// Update strategy: Update the existing object, if changes are identified in the desired.Data["nodes"]
-	updated, err := r.ReconcileConfigMap(ctx, ts)
+	configMapUpdated, err := r.ReconcileConfigMap(ctx, ts)
 	if err != nil {
 		cerr := r.setConditionNotReady(ctx, &ts, ConditionReasonConfigMapNotReady, err)
 		if cerr != nil {
@@ -173,7 +182,6 @@ func (r *TypesenseClusterReconciler) Reconcile(ctx context.Context, req ctrl.Req
 	}
 
 	// Update strategy: Update the whole specs when changes are identified
-	// Update the whole specs when changes are identified
 	sts, err := r.ReconcileStatefulSet(ctx, &ts)
 	if err != nil {
 		cerr := r.setConditionNotReady(ctx, &ts, ConditionReasonStatefulSetNotReady, err)
@@ -184,69 +192,90 @@ func (r *TypesenseClusterReconciler) Reconcile(ctx context.Context, req ctrl.Req
 	}
 
 	terminationGracePeriodSeconds := *sts.Spec.Template.Spec.TerminationGracePeriodSeconds
+	requeueAfter := reconcileRequeuePeriod + (time.Duration(terminationGracePeriodSeconds) * time.Second)
 	toTitle := func(s string) string {
 		return cases.Title(language.Und, cases.NoLower).String(s)
 	}
 
 	cond := ConditionReasonQuorumStateUnknown
-	if *updated {
-		condition, _, err := r.ReconcileQuorum(ctx, &ts, secret, client.ObjectKeyFromObject(sts))
+	action := Bootstrapping
+	if configMapUpdated != nil {
+		action = Reconciling
+	}
+
+	if configMapUpdated == nil {
+		r.logger.Info(fmt.Sprintf("%s cluster completed", string(action)), "condition", cond, "requeueAfter", requeueAfter)
+		return ctrl.Result{RequeueAfter: requeueAfter}, nil
+	}
+
+	if *configMapUpdated {
+		err = r.forcePodsConfigMapUpdate(ctx, &ts)
 		if err != nil {
-			r.logger.Error(err, "reconciling quorum health failed")
+			r.logger.Error(err, "failed to force pods configmap update", "configmap", fmt.Sprintf(ClusterNodesConfigMap, ts.Name))
 		}
 
-		if strings.Contains(string(condition), "QuorumNeedsAttention") {
-			eram := "cluster needs manual administrative attention: "
+		cond = ConditionReasonQuorumNotReadyWaitATerm
+		requeueAfter = configMapRequeuePeriod
 
-			if condition == ConditionReasonQuorumNeedsAttentionClusterIsLagging {
-				eram += "queued_writes > healthyWriteLagThreshold"
+		err = errors.New("wait on configmap updates")
+		cerr := r.setConditionNotReady(ctx, &ts, string(cond), err)
+		if cerr != nil {
+			return ctrl.Result{}, cerr
+		}
+		r.Recorder.Eventf(&ts, "Warning", string(cond), toTitle(err.Error()))
+	}
+
+	condition, _, err := r.ReconcileQuorum(ctx, &ts, secret, client.ObjectKeyFromObject(sts))
+	if err != nil {
+		r.logger.Error(err, "reconciling quorum health failed")
+	}
+
+	if strings.Contains(string(condition), "QuorumNeedsAttention") {
+		eram := "cluster needs manual administrative attention: "
+
+		if condition == ConditionReasonQuorumNeedsAttentionClusterIsLagging {
+			eram += "queued_writes > healthyWriteLagThreshold"
+		}
+
+		if condition == ConditionReasonQuorumNeedsAttentionMemoryOrDiskIssue {
+			eram += "out of memory or disk"
+		}
+
+		erram := errors.New(eram)
+		cerr := r.setConditionNotReady(ctx, &ts, string(condition), erram)
+		if cerr != nil {
+			return ctrl.Result{}, cerr
+		}
+		r.Recorder.Eventf(&ts, "Warning", string(condition), toTitle(erram.Error()))
+
+	} else {
+		if condition != ConditionReasonQuorumReady {
+			if err == nil {
+				err = errors.New("quorum is not ready")
 			}
-
-			if condition == ConditionReasonQuorumNeedsAttentionMemoryOrDiskIssue {
-				eram += "out of memory or disk"
-			}
-
-			erram := errors.New(eram)
-			cerr := r.setConditionNotReady(ctx, &ts, string(condition), erram)
+			cerr := r.setConditionNotReady(ctx, &ts, string(condition), err)
 			if cerr != nil {
 				return ctrl.Result{}, cerr
 			}
-			r.Recorder.Eventf(&ts, "Warning", string(condition), toTitle(erram.Error()))
 
+			r.Recorder.Eventf(&ts, "Warning", string(condition), toTitle(err.Error()))
 		} else {
-			if condition != ConditionReasonQuorumReady {
-				if err == nil {
-					err = errors.New("quorum is not ready")
-				}
-				cerr := r.setConditionNotReady(ctx, &ts, string(condition), err)
-				if cerr != nil {
-					return ctrl.Result{}, cerr
-				}
+			report := ts.Status.Conditions[0].Status != metav1.ConditionTrue
 
-				r.Recorder.Eventf(&ts, "Warning", string(condition), toTitle(err.Error()))
-			} else {
-				report := ts.Status.Conditions[0].Status != metav1.ConditionTrue
+			cerr := r.setConditionReady(ctx, &ts, string(condition))
+			if cerr != nil {
+				return ctrl.Result{}, cerr
+			}
 
-				cerr := r.setConditionReady(ctx, &ts, string(condition))
-				if cerr != nil {
-					return ctrl.Result{}, cerr
-				}
-
-				if report {
-					r.Recorder.Eventf(&ts, "Normal", string(condition), toTitle("quorum is ready"))
-				}
+			if report {
+				r.Recorder.Eventf(&ts, "Normal", string(condition), toTitle("quorum is ready"))
 			}
 		}
-		cond = condition
 	}
 
-	lastAction := "bootstrapping"
-	if *updated {
-		lastAction = "reconciling"
-	}
-	requeueAfter = time.Duration(60+terminationGracePeriodSeconds) * time.Second
-	r.logger.Info(fmt.Sprintf("%s cluster completed", lastAction), "condition", cond, "requeueAfter", requeueAfter)
+	cond = condition
 
+	r.logger.Info(fmt.Sprintf("%s cluster completed", string(action)), "condition", cond, "requeueAfter", requeueAfter)
 	return ctrl.Result{RequeueAfter: requeueAfter}, nil
 }
 

internal/controller/typesensecluster_helpers.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"context"
+
 	tsv1alpha1 "github.com/akyriako/typesense-operator/api/v1alpha1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )

internal/controller/typesensecluster_quorum.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"sort"
 	"strconv"
+	"strings"
 	"time"
 
 	tsv1alpha1 "github.com/akyriako/typesense-operator/api/v1alpha1"
@@ -89,6 +90,10 @@ func (r *TypesenseClusterReconciler) ReconcileQuorum(ctx context.Context, ts *ts
 	r.logger.V(debugLevel).Info("reporting cluster status", "status", clusterStatus)
 
 	if clusterStatus == ClusterStatusSplitBrain {
+		nodeslist := strings.Split(quorum.NodesListConfigMap.Data["nodeslist"], ",")
+		if _, c := contains(nodeslist, fmt.Sprintf(ClusterStatefulSet, ts.Name)); c == true {
+			return ConditionReasonQuorumNotReadyWaitATerm, 0, nil
+		}
 		return r.downgradeQuorum(ctx, ts, quorum.NodesListConfigMap, stsObjectKey, sts.Status.ReadyReplicas, int32(quorum.MinRequiredNodes))
 	}
 
@@ -142,6 +147,10 @@ func (r *TypesenseClusterReconciler) ReconcileQuorum(ctx context.Context, ts *ts
 	}
 
 	if clusterStatus == ClusterStatusElectionDeadlock {
+		nodeslist := strings.Split(quorum.NodesListConfigMap.Data["nodeslist"], ",")
+		if _, c := contains(nodeslist, fmt.Sprintf(ClusterStatefulSet, ts.Name)); c == true {
+			return ConditionReasonQuorumNotReadyWaitATerm, 0, nil
+		}
 		return r.downgradeQuorum(ctx, ts, quorum.NodesListConfigMap, stsObjectKey, int32(healthyNodes), int32(minRequiredNodes))
 	}
 
@@ -216,7 +225,7 @@ func (r *TypesenseClusterReconciler) downgradeQuorum(
 		return ConditionReasonQuorumNotReady, 0, err
 	}
 
-	_, size, err := r.updateConfigMap(ctx, ts, cm, ptr.To[int32](desiredReplicas))
+	_, size, _, err := r.updateConfigMap(ctx, ts, cm, ptr.To[int32](desiredReplicas), true)
 	if err != nil {
 		return ConditionReasonQuorumNotReady, 0, err
 	}
@@ -246,7 +255,7 @@ func (r *TypesenseClusterReconciler) upgradeQuorum(
 		return ConditionReasonQuorumNotReady, 0, err
 	}
 
-	_, _, err = r.updateConfigMap(ctx, ts, cm, &size)
+	_, _, _, err = r.updateConfigMap(ctx, ts, cm, &size, true)
 	if err != nil {
 		return ConditionReasonQuorumNotReady, 0, err
 	}

internal/controller/typesensecluster_statefulset.go
@@ -5,6 +5,10 @@ import (
 	"crypto/sha256"
 	"encoding/json"
 	"fmt"
+	"strconv"
+	"strings"
+	"time"
+
 	tsv1alpha1 "github.com/akyriako/typesense-operator/api/v1alpha1"
 	"github.com/mitchellh/hashstructure/v2"
 	appsv1 "k8s.io/api/apps/v1"
@@ -15,9 +19,6 @@ import (
 	"k8s.io/utils/ptr"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"strconv"
-	"strings"
-	"time"
 )
 
 const (
@@ -99,7 +100,7 @@ func (r *TypesenseClusterReconciler) ReconcileStatefulSet(ctx context.Context, t
 					r.logger.V(debugLevel).Error(err, fmt.Sprintf("unable to fetch config map: %s", configMapName))
 				}
 
-				_, _, err = r.updateConfigMap(ctx, ts, cm, updatedSts.Spec.Replicas)
+				_, _, _, err = r.updateConfigMap(ctx, ts, cm, updatedSts.Spec.Replicas, true)
 				if err != nil {
 					r.logger.V(debugLevel).Error(err, fmt.Sprintf("unable to update config map: %s", configMapName))
 				}

commit 80d0287b6c790acc6c8f44aa5ec06273719c7a71

draft fix for bug introduced in #164 #223

Makefile
@@ -52,7 +52,7 @@ OPERATOR_SDK_VERSION ?= v1.38.0
 # Image URL to use all building/pushing image targets
 DOCKER_HUB_NAME ?= akyriako78#$(shell docker info | sed '/Username:/!d;s/.* //')
 IMG_NAME ?= typesense-operator
-IMG_TAG ?= 0.3.6-1.dev.1
+IMG_TAG ?= 0.3.6-1.dev.2
 IMG ?= $(DOCKER_HUB_NAME)/$(IMG_NAME):$(IMG_TAG)
 
 # ENVTEST_K8S_VERSION refers to the version of kubebuilder assets to be downloaded by envtest binary.

config/samples/ts_v1alpha1_typesensecluster_kind.yaml
@@ -62,7 +62,7 @@ metadata:
   name: c-kind-2
 spec:
   image: typesense/typesense:30.0.rc29-amd64
-  replicas: 5
+  replicas: 3
   storage:
     size: 150Mi
     storageClassName: typesense-local-path

internal/controller/typesensecluster_configmap.go
@@ -13,6 +13,7 @@ import (
 	discoveryv1 "k8s.io/api/discovery/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/labels"
+	utilerrors "k8s.io/apimachinery/pkg/util/errors"
 	"k8s.io/utils/ptr"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -182,21 +183,28 @@ func (r *TypesenseClusterReconciler) forcePodsConfigMapUpdate(ctx context.Contex
 		return err
 	}
 
+	var errs []error
 	for i := range podList.Items {
 		pod := &podList.Items[i]
+		original := pod.DeepCopy()
+
+		if pod.DeletionTimestamp.IsZero() {
+			continue
+		}
 
 		if pod.Annotations == nil {
 			pod.Annotations = map[string]string{}
 		}
 		pod.Annotations[forceConfigMapUpdateAnnotationKey] = time.Now().Format(time.RFC3339)
 
-		if err := r.Patch(ctx, pod, client.MergeFrom(pod.DeepCopy())); err != nil {
-			r.logger.Error(err, "patching to pod annotations failed", "pod", pod.Name)
-			return err
+		if err := r.Patch(ctx, pod, client.MergeFrom(original)); err != nil {
+			r.logger.Error(err, "patching pod annotations failed", "pod", pod.Name)
+			errs = append(errs, fmt.Errorf("pod %s: %w", pod.Name, err))
+			continue
 		}
 	}
 
-	return nil
+	return utilerrors.NewAggregate(errs)
 }
 
 func (r *TypesenseClusterReconciler) getNodes(ctx context.Context, ts *tsv1alpha1.TypesenseCluster, replicas int32, bootstrapping bool) ([]string, error) {