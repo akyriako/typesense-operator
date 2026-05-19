2cbfce33f0365871973a1464cb511c3c9d761104
Kyriakos Akriotis <akriotis.kyriakos@gmail.com>
2026-05-19 12:15:28 +0200

    added autogenerated config for scrapers, added host for scrapers and … (#271)
    * added autogenerated config for scrapers, added host for scrapers and user-defined image #268
    
    * bump manifests and charts #268

--- api/v1alpha1/typesensecluster_types_scraper.go
+++ api/v1alpha1/typesensecluster_types_scraper.go
@@ -1,11 +1,31 @@
 package v1alpha1
 
-import corev1 "k8s.io/api/core/v1"
+import (
+	"encoding/json"
+	"fmt"
+
+	corev1 "k8s.io/api/core/v1"
+)
 
 type DocSearchScraperSpec struct {
-	Name   string `json:"name"`
-	Image  string `json:"image"`
-	Config string `json:"config"`
+	Name string `json:"name"`
+
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:default:="typesense/docsearch-scraper:0.12.0.rc14"
+	Image string `json:"image,omitempty"`
+
+	// +optional
+	// +kubebuilder:default=https
+	// +kubebuilder:validation:Type=string
+	// +kubebuilder:validation:Enum=http;https
+	Protocol string `json:"protocol,omitempty"`
+
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:validation:Pattern:=`^([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])(\.([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]{0,61}[a-zA-Z0-9]))*$`
+	Host *string `json:"host,omitempty"`
+
+	// +kubebuilder:validation:Optional
+	Config *string `json:"config,omitempty"`
 
 	// +kubebuilder:validation:Pattern:=`(^((\*\/)?([0-5]?[0-9])((\,|\-|\/)([0-5]?[0-9]))*|\*)\s+((\*\/)?((2[0-3]|1[0-9]|[0-9]|00))((\,|\-|\/)(2[0-3]|1[0-9]|[0-9]|00))*|\*)\s+((\*\/)?([1-9]|[12][0-9]|3[01])((\,|\-|\/)([1-9]|[12][0-9]|3[01]))*|\*)\s+((\*\/)?([1-9]|1[0-2])((\,|\-|\/)([1-9]|1[0-2]))*|\*|(jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|des))\s+((\*\/)?[0-6]((\,|\-|\/)[0-6])*|\*|00|(sun|mon|tue|wed|thu|fri|sat))\s*$)|@(annually|yearly|monthly|weekly|daily|hourly|reboot)`
 	// +kubebuilder:validation:Type=string
@@ -27,4 +47,72 @@ func (s *DocSearchScraperSpec) GetScraperAuthConfiguration() []corev1.EnvFromSou
 	}
 
 	return []corev1.EnvFromSource{}
+}
+
+func (s *DocSearchScraperSpec) GetScraperConfig() string {
+	if s.Config != nil {
+		return *s.Config
+	}
+
+	if s.Host == nil {
+		return ""
+	}
+
+	host := *s.Host
+	config := struct {
+		IndexName             string                 `json:"index_name"`
+		StartURLs             []string               `json:"start_urls"`
+		SitemapURLs           []string               `json:"sitemap_urls"`
+		SitemapAlternateLinks bool                   `json:"sitemap_alternate_links"`
+		StopURLs              []string               `json:"stop_urls"`
+		Selectors             map[string]interface{} `json:"selectors"`
+		StripChars            string                 `json:"strip_chars"`
+		CustomSettings        map[string]interface{} `json:"custom_settings"`
+	}{
+		IndexName:             s.Name,
+		StartURLs:             []string{fmt.Sprintf("%s://%s", s.Protocol, host)},
+		SitemapURLs:           []string{fmt.Sprintf("%s://%s/sitemap.xml", s.Protocol, host)},
+		SitemapAlternateLinks: false,
+		StopURLs:              []string{"/tests"},
+		StripChars:            " .,;:#",
+
+		Selectors: map[string]interface{}{
+			"lvl0": map[string]interface{}{
+				"selector":      "nav.menu a.menu__link--active, .navbar__item.navbar__link--active",
+				"default_value": "Documentation",
+			},
+			"lvl1": "article h1, header h1",
+			"lvl2": "article h2",
+			"lvl3": "article h3",
+			"lvl4": "article h4",
+			"lvl5": "article h5",
+			"lvl6": "article h6",
+			"text": "article p, article li, article td",
+		},
+
+		CustomSettings: map[string]interface{}{
+			"separatorsToIndex": "_",
+			"attributesForFaceting": []string{
+				"language",
+				"version",
+				"type",
+				"docusaurus_tag",
+			},
+			"attributesToRetrieve": []string{
+				"hierarchy",
+				"content",
+				"anchor",
+				"url",
+				"url_without_anchor",
+				"type",
+			},
+		},
+	}
+
+	configJson, err := json.Marshal(config)
+	if err != nil {
+		return ""
+	}
+
+	return string(configJson)
 }
